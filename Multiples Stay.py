5d876356bb0f0e26169b590b711dc6a2f155cc4e
MichaelJrTheUltimate <mwnoggins24@coe.edu>
2025-03-14 16:45:11 +0000

    Multi

--- Multiples Stay.py
+++ Multiples Stay.py
@@ -2,7 +2,44 @@ import random
 
 def rollDice():
     return random.randint(1, 6)
-def Rolling():
-    Rolls = [rollDice() for _ in range(10)]
-    Score = sum(Rolls)
-    return Score
+
+def Rolling(remaining_rolls, player_name):
+    unique_rolls = set()
+
+    Rolls = [rollDice() for _ in range(remaining_rolls)]
+    print(f"{player_name} rolled: ", Rolls)
+        
+    for roll in Rolls:
+        if roll in unique_rolls:
+            remaining_rolls -= 1
+        else:
+            unique_rolls.add(roll)
+        
+        print(f"{player_name} has", remaining_rolls, "rolls left")
+    
+    return remaining_rolls
+
+def main():
+    Player1Rolling = True
+    Player2Rolling = False
+    remaining_rolls = 10
+
+    while remaining_rolls > 1:
+        if Player1Rolling:
+            print("Player 1's turn:")
+            remaining_rolls = Rolling(remaining_rolls, "Player 1")
+            Player1Rolling = False
+            Player2Rolling = True
+        elif Player2Rolling:
+            print("Player 2's turn:")
+            remaining_rolls = Rolling(remaining_rolls, "Player 2")
+            Player2Rolling = False
+            Player1Rolling = True
+
+    if remaining_rolls == 1 or remaining_rolls == 0:
+        if Player1Rolling:
+            print("Player 1 wins!")
+        else:
+            print("Player 2 wins!")
+
+main()
